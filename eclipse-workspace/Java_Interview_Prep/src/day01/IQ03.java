4ba23413a2a539bae09e7140f96f98f240896e13
sekercih <hkkskrc@gmail.com>
2021-04-17 17:13:34 +0300

    vol1

---
+++ eclipse-workspace/Java_Interview_Prep/src/day01/IQ03.java
@@ -0,0 +1,39 @@
+package day01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+
+
+public class IQ03 {
+
+	//Create a method which returns non-repeated elements from a list
+	
+		public static void main(String[] args) {
+			
+			Integer arr[] = {2,3,3,5,8,6,99,2,1,14};
+			
+			List<Integer> nums = new ArrayList<>();
+			//1.way
+			Collections.addAll(nums, arr);
+			
+			
+			//2. way
+			nums = Arrays.stream(arr).collect(Collectors.toList());
+			
+			System.out.println(nums);
+			
+			HashSet<Integer> hs = new HashSet<>(nums);
+			System.out.println(hs.toString());
+			
+			
+			
+		}
+
+	}
---
+++ eclipse-workspace/Java_Interview_Prep/src/day01/IQ1.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class IQ1 {
+	/*
+		Type a program to print the numbers from 0 to 100 without using any integer
+		integer deðiþkeni kullanmadan, 0 da 100 e kadar sayýlarý bastýran bir program yazýnýz.
+*/
+	public static void main(String[] args) {
+		for (char i = (' '-32);i<'e';i++) {
+			System.out.print((int)i+" ");
+		}
+char elma=240;
+System.out.println();
+System.out.println((int)elma);
+	}
+
+}
